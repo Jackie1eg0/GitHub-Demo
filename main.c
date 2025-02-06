b67bd8e0895b84a33f9f502f58957b585c4969b4
Jackie137 <jackie1eg0137@gmail.com>
2025-02-06 15:37:19 +0800

    第三次提交

--- main.c
+++ main.c
@@ -12,3 +12,12 @@ struct node* createNode(int data) {
     newNode->right = NULL;
     return newNode;
 }
+struct node* insert(struct node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
