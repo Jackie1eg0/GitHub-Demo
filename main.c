1ae25a11df1927a1b1e137e4ac7ce27efa72cfc7
Jackie137 <jackie1eg0137@gmail.com>
2025-02-06 14:40:49 +0800

    second commit

--- main.c
+++ main.c
@@ -5,3 +5,10 @@ struct node {
     struct node* left;
     struct node* right;
 };
+struct node* createNode(int data) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
